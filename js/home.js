bdefc4d4b4bda102596c143833505a3656d6ca1f
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2024-04-18 12:17:07 +0000

    js

--- js/home.js
+++ js/home.js
@@ -1,13 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Toggle description visibility on profile click
-    const profileInfos = document.querySelectorAll(".profile-info");
-    profileInfos.forEach((profileInfo) => {
-      profileInfo.addEventListener("click", function () {
-        profileInfos.forEach((item) => {
-          item.classList.remove("active");
-        });
-        this.classList.toggle("active");
-      });
-    });
-  });
-  
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
